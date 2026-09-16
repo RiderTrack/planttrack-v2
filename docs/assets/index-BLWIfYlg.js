613ae731846b4922c209c900d2c2e6b5f4249432
RiderTrack <288009156+RiderTrack@users.noreply.github.com>
2026-09-16 01:54:59 +0000

    🌐 deploy: web build a docs/ (GitHub Pages)

---
+++ docs/assets/index-BLWIfYlg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-nhV-P9z1.js","./index-CztDSaO0.js","./index-FA3w21ok.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CztDSaO0.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-nhV-P9z1.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
---
+++ docs/assets/index-gw6U9j_W.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CGtMvoCp.js","./index-CztDSaO0.js","./index-FA3w21ok.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-CztDSaO0.js";const o=r("App",{web:()=>p(()=>import("./web-CGtMvoCp.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
