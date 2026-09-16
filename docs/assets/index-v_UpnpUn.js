77a4b39c99c94c5a925c3b5d15775a758de8e5c0
RiderTrack <288009156+RiderTrack@users.noreply.github.com>
2026-09-16 01:08:03 +0000

    🌐 deploy: web build a docs/ (GitHub Pages)

---
+++ docs/assets/index-v_UpnpUn.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BpXkuaAN.js","./index-BVdYAIze.js","./index-DSzubknF.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-BVdYAIze.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-BpXkuaAN.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
